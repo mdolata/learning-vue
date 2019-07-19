3d98548dd96f0028d60b77a48acc53164a45b306
mdolata <mateuszdolata2@gmail.com>
2019-07-19 22:50:18 +0200

    simple form validation

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,11 +1,29 @@
 <template>
   <div id="employee-form">
     <form @submit.prevent="handleSubmit">
-      <label>Employee name</label>
-      <input v-model="employee.name" type="text" />
-      <label>Employee Email</label>
-      <input v-model="employee.email" type="text" />
-      <button>Add Employee</button>
+        <label>Employee name</label>
+        <input
+            ref="first"
+            type="text"
+            :class="{ 'has-error': submitting && invalidName }"
+            v-model="employee.name"
+            @focus="clearStatus"
+            @keypress="clearStatus"
+        />
+        <label>Employee Email</label>
+        <input
+            type="text"
+            :class="{ 'has-error': submitting && invalidEmail }"
+            v-model="employee.email"
+            @focus="clearStatus"
+        />
+        <p v-if="error && submitting" class="error-message">
+            ❗Please fill out all required fields
+        </p>
+        <p v-if="success" class="success-message">
+            ✅ Employee successfully added
+        </p>
+        <button>Add Employee</button>
     </form>
   </div>
 </template>
@@ -15,22 +33,68 @@
     name: 'employee-form',
     data() {
       return {
-        employee: {
-          name: '',
-          email: '',
-        },
+        submitting: false,
+        error: false,
+        success: false,
+        employee: this.emptyEmployee(),
       }
     },
     methods: {
         handleSubmit() {
+            this.submitting = true
+            this.clearStatus()
+
+            if(this.invalidName || this.invalidEmail){
+                this.error = true
+                console.log("error")
+                return
+            }
+
             this.$emit('add:employee', this.employee)
+            this.$refs.first.focus()
+            this.employee = this.emptyEmployee()
+
+            this.error = false
+            this.success = true
+            this.submitting = false
+        },
+        clearStatus() {
+            this.success = false
+            this.error = false
+        },
+        emptyEmployee(){
+            return {
+                name: '',
+                email: '',
+              }
+        }
+        
+    },
+    computed: {
+        invalidName() {
+            return this.employee.name === ''
+        },
+        invalidEmail() {
+            return this.employee.email === ''
         }
     }
   }
 </script>
 
 <style scoped>
-  form {
-    margin-bottom: 2rem;
-  }
+form {
+  margin-bottom: 2rem;
+}
+
+[class*='-message'] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
+}
 </style>
