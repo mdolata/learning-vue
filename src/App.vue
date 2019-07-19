c859b230f06de010c0d4d95954477270b877ae4f
mdolata <mateuszdolata2@gmail.com>
2019-07-19 22:25:37 +0200

    add new employee form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <employee-form @add:employee="addEmployee" />
     <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
 import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'app',
   components: {
-    EmployeeTable
+    EmployeeTable,
+    EmployeeForm
   },
   data() {
     return {
@@ -32,6 +35,18 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    addEmployee(employee) {
+      let lastId = this.employees
+        .map(function(e) { return e.id; })
+        .reduce(function(currentMax, number) {
+          return number > currentMax ? number : currentMax;
+        }, 1)
+      let id = lastId + 1
+      let newEmployee = { ...employee, id }
+      this.employees = [...this.employees, newEmployee]
+    },
   }
 }
 </script>
